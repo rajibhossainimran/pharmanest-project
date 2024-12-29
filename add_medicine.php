da2808e38e5f42234eb5805c0d97bba48fb9d245
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-30 01:07:40 +0600

    upload field add

--- add_medicine.php
+++ add_medicine.php
@@ -20,9 +20,9 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="mb-3">
-                        <label for="medicineImage" class="form-label">Medicine Image</label>
-                        <input type="file" class="form-control" id="medicineImage" name="medicine_image" required>
+                <div class="mb-3">
+                        <label for="shelfNo" class="form-label">Shelf No</label>
+                        <input type="text" class="form-control" id="shelfNo" name="shelf_no" required>
                     </div>
                 </div>
             </div>
@@ -60,12 +60,6 @@
             <div class="row mb-4">
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label for="shelfNo" class="form-label">Shelf No</label>
-                        <input type="text" class="form-control" id="shelfNo" name="shelf_no" required>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="mb-3">
                         <label for="medicineStatus" class="form-label">Medicine Status</label>
                         <select class="form-control" id="medicineStatus" name="medicine_status">
                             <option value="available">Available</option>
@@ -73,17 +67,55 @@
                         </select>
                     </div>
                 </div>
+                <div class="col-md-6">
+                <div class="row mb-4">
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="medicineImage" class="form-label">Upload Image</label>
+                            <input type="file" class="form-control" id="medicineImage" name="medicine_image" accept="image/*" required>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-center border rounded p-3" style="min-height: 200px;">
+                            <p class="text-muted" id="previewText">No image selected</p>
+                            <img id="imagePreview" src="#" alt="Preview" class="img-fluid d-none" style="max-height: 200px;">
+                        </div>
+                    </div>
+                </div>
+                  </div>
             </div>
 
             <div class="row">
                 <div class="col-4"></div>
                 <div class="col-4 text-center">
-                    <button type="submit" class="btn btn-success">Submit</button>
+                    <button type="submit" class="btn btn-success">Add Medicine</button>
                 </div>
                 <div class="col-4"></div>
             </div>
         </form>
     </div>
 </main>
+<script>
+    const medicineImage = document.getElementById('medicineImage');
+    const imagePreview = document.getElementById('imagePreview');
+    const previewText = document.getElementById('previewText');
 
+    medicineImage.addEventListener('change', function () {
+        const file = this.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            previewText.classList.add('d-none');
+            imagePreview.classList.remove('d-none');
+
+            reader.onload = function (e) {
+                imagePreview.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        } else {
+            imagePreview.classList.add('d-none');
+            previewText.classList.remove('d-none');
+        }
+    });
+</script>
 <?php include("./pages/common_pages/footer.php"); ?>
--- pages/common_pages/sidebar.php
+++ pages/common_pages/sidebar.php
@@ -33,7 +33,7 @@
                                 </p>
                             </a>
                             <ul class="nav nav-treeview">
-                            <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                            <li class="nav-item"> <a href="add_medicine.php" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
                                         <p>Add New Medicine</p>
                                     </a> </li>
                                     <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>

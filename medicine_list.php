038084b07c22490cbf6c50948ab7237b1919fac1
rajibhossainimran <rajibhossainimran@gmail.com>
2025-01-05 23:40:48 +0600

    medicine display table

--- medicine_list.php
+++ medicine_list.php
@@ -19,34 +19,38 @@ $result = $db->query($sql);
 <main class="app-main">
     <div class="container mt-2 mb-5">
     <h1>Medicine List</h1>
-    <table>
-        <thead>
+    <table class="table table-bordered table-hover table-striped">
+        <thead class="table-dark">
             <tr>
-                <th>ID</th>
+                <th>SL</th>
                 <th>Medicine Name</th>
-                <th>Manufacturer</th>
+                <th>Image</th>
+                <th>Company</th>
                 <th>Type</th>
                 <th>Supplier</th>
                 <th>Status</th>
-                <th>Image</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
             <?php
+            $count = 0;
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
+                    $count++;
                     echo "<tr>
-                        <td>{$row['id']}</td>
+                        <td>$count</td>
                         <td>{$row['m_name']}</td>
+                        <td><img src='{$row['medicine_image']}' alt='Medicine Image' class='img-thumbnail' style='max-width: 100px;'></td>
                         <td>{$row['manufacturer']}</td>
                         <td>{$row['m_type']}</td>
                         <td>{$row['supplier']}</td>
                         <td>{$row['status']}</td>
-                        <td><img src='{$row['medicine_image']}' alt='Medicine Image'></td>                        
+                                                
                     </tr>";
                 }
             } else {
-                echo "<tr><td colspan='8'>No data found</td></tr>";
+                echo "<tr><td colspan='7' class='text-center'>No data found</td></tr>";
             }
             ?>
         </tbody>
